c57b87fb9cf92f3dab4bd73ec203acc22b52e813
rafael.wassoaski <meireles200@hotmail.com>
2024-06-09 22:13:44 -0300

    Corrigindo teste

--- src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
+++ src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
@@ -3,10 +3,8 @@ package br.com.alura.adopet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AdopetApiApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
